06e8003585a57e6862d446b3cabdf763d07b75be
Manish Timalsina <manishtimalsina51@gmail.com>
2024-06-17 00:52:16 +0545

    user edit and update complete

---
+++ resources/views/role-permission/user/edit.blade.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>Users</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h4>Edit Users
+                            <a href="{{ url('users') }}" class="btn btn-danger float-right" >Back</a>
+                        </h4>
+                    </div>
+                    <div class="card-body">
+                        <form action="{{ url('users/'.$user->id)  }}" method="POST">
+                            @csrf
+                            @method('PUT')
+                            <div class="mb-3">
+                                <label for="">User Name</label>
+                                <input type="text" name="name" value="{{ $user->name }}" class="form-control" />
+                                @error('name') <span class="text-danger">{{ $message }}</span> @enderror
+                            </div>
+                            <div class="mb-3">
+                                <label for="">Email</label>
+                                <input type="text" name="email" value="{{ $user->email }}" readonly class="form-control" />
+                               
+                            </div>
+                            <div class="mb-3">
+                                <label for="">Password</label>
+                                <input type="password" name="password"  class="form-control" />
+                                @error('password') <span class="text-danger">{{ $message }}</span> @enderror
+                            </div>
+                            <div class="mb-3">
+                                <label for="">Roles</label>
+                                <select  name="roles[]" class="form-control" multiple> 
+                                    <option value="">Select Roles</option>
+                                    @foreach($roles as $role)
+                                        <option 
+                                        value="{{ $role }}"
+                                        {{ in_array($role, $userRoles) ? 'selected' : "" }}
+                                        >
+                                        {{ $role }}</option>
+
+                                    @endforeach
+                                </select>
+                                @error('roles') <span class="text-danger">{{ $message }}</span> @enderror
+                            </div>
+                            <div class="mb-3">
+                                <button type="submit" class="btn btn-primary" >Update</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
