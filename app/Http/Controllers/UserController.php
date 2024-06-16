b0a8ddc6db053485ad73423ee67c43f43dc54fe6
Manish Timalsina <manishtimalsina51@gmail.com>
2024-06-17 00:52:54 +0545

    added all functionality for user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,7 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
-
+use Spatie\Permission\Models\Role;
+use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     public function index(){
@@ -14,6 +15,64 @@ class UserController extends Controller
     }
 
     public function create(){
-        return view('role-permission.user.create');
+        $roles =Role::pluck('name','name')->all();
+        return view('role-permission.user.create',compact('roles'));
     }
+
+    public function store(Request $request){
+        $request->validate([
+            'name'=>'required|string|max:255',
+            'email'=>'required|email|max:255|unique:users,email',
+            'password'=>'required|string|min:8|max:20',
+            'roles'=>'required',
+        ]);
+
+        $user = User::create([
+                    'name' => $request->name,
+                    'email'=> $request->email,
+                    'password'=> Hash::make($request->password),
+        ]);
+        $user->syncRoles($request->roles);
+
+        return redirect('/users')->with('status','Users created successfully with Roles'); 
+}
+
+public function edit(User $user){
+    $roles =Role::pluck('name','name')->all();
+    $userRoles = $user->roles->pluck('name','name')->all();
+    return view('role-permission.user.edit',compact('user','roles','userRoles'));
+}
+
+public function update(Request $request, User $user){
+    $request->validate([
+        'name'=>'required|string|max:255',
+        'password'=>'nullable|string|min:8|max:20',
+        'roles'=>'required',
+    ]);
+
+
+    $data = [
+                    'name' => $request->name,
+                    'email'=> $request->email,
+                    'password'=> Hash::make($request->password),
+    ];
+
+    if(!empty($request->password)){
+        $data += [
+            'password'=> Hash::make($request->password), 
+        ];
+    }
+    $user->update($data);
+    $user->syncRoles($request->roles);
+
+    return redirect('/users')->with('status','Users Updated successfully with Roles'); 
+}
+
+public function delete($userId){
+    $user = User::findOrFail($userId);
+    $user->delete();
+
+    return redirect('/users')->with('status','Users deleted successfully  along with Roles'); 
+
+}
 }
