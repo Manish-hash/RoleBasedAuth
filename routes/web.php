46a9f5c5e77d4825c8b155f9ecad47399b2a350a
Manish Timalsina <manishtimalsina51@gmail.com>
2024-06-17 00:53:29 +0545

    routes uses middleware

--- routes/web.php
+++ routes/web.php
@@ -9,12 +9,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('permissions', PermissionController::class );
-Route::get('permissions/{id}/delete', [PermissionController::class,'delete']);
-
-Route::resource('roles', RoleController::class );
-Route::get('roles/{id}/delete', [RoleController::class,'delete']);
-Route::get('roles/{id}/give-permissions', [RoleController::class,'addPermissionToRole']);
-Route::put('roles/{id}/give-permissions', [RoleController::class,'givePermissionToRole']);
-
-Route::resource('users', UserController::class );
+Route::group(['middleware' => 'auth'], function(){
+    Route::resource('permissions', PermissionController::class );
+    Route::get('permissions/{id}/delete', [PermissionController::class,'delete']);
+    
+    Route::resource('roles', RoleController::class );
+    Route::get('roles/{id}/delete', [RoleController::class,'delete']);
+    Route::get('roles/{id}/give-permissions', [RoleController::class,'addPermissionToRole']);
+    Route::put('roles/{id}/give-permissions', [RoleController::class,'givePermissionToRole']);
+    
+    Route::resource('users', UserController::class );
+    Route::get('users/{userId}/delete', [UserController::class,'delete']);
+});
