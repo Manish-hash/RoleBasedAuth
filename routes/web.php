5fb9793ac30ec45e8f7095064fdf919e55a52aa8
Manish Timalsina <manishtimalsina51@gmail.com>
2024-06-17 12:09:08 +0545

    laravel breeze package installation

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PermissionController;
 use App\Http\Controllers\RoleController;
@@ -9,7 +10,16 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::group(['middleware' => 'auth'], function(){
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+
     Route::resource('permissions', PermissionController::class );
     Route::get('permissions/{id}/delete', [PermissionController::class,'delete']);
     
@@ -21,3 +31,7 @@ Route::group(['middleware' => 'auth'], function(){
     Route::resource('users', UserController::class );
     Route::get('users/{userId}/delete', [UserController::class,'delete']);
 });
+
+
+
+require __DIR__.'/auth.php';
